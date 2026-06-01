e2a8a6add6b146ce87d47f4fa2560a2c7157208e
Anant Chauhan <anantchauhan2006@gmail.com>
2026-06-01 14:01:43 +0530

    Update solution.cpp

--- easy/001-two-sum/solution.cpp
+++ easy/001-two-sum/solution.cpp
@@ -1,1 +1,23 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            auto it = seen.find(complement);
+            if (it != seen.end()) {
+                return {it->second, i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
