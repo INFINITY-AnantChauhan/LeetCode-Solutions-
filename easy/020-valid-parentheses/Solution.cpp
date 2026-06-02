973fddce7a0cc18cc5f6a86a9ae1aa55abdc4d6d
Anant Chauhan <anantchauhan2006@gmail.com>
2026-06-02 10:37:38 +0530

    Update Solution.cpp

--- easy/020-valid-parentheses/Solution.cpp
+++ easy/020-valid-parentheses/Solution.cpp
@@ -1,1 +1,28 @@
+#include <stack>
+#include <unordered_map>
+using namespace std;
 
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        unordered_map<char, char> match = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.empty() || st.top() != match[c])
+                    return false;
+                st.pop();
+            }
+        }
+        
+        return st.empty(); 
+    }
+};
